5236e361c4fe0146cf0b8a0aae21fd001a9cc940
David Long <david.long@snows.world>
2025-05-16 01:16:51 -0400

    fix: move away from autowired

--- src/main/java/ca/codeglacier/makishima/service/DiscordUserService.java
+++ src/main/java/ca/codeglacier/makishima/service/DiscordUserService.java
@@ -2,14 +2,16 @@ package ca.codeglacier.makishima.service;
 
 import ca.codeglacier.makishima.entity.DiscordUser;
 import ca.codeglacier.makishima.repository.DiscordUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiscordUserService {
 
-    @Autowired
-    private DiscordUserRepository userRepository;
+    private final DiscordUserRepository userRepository;
+
+    public DiscordUserService(DiscordUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public void save(DiscordUser user) {
         userRepository.save(user);
